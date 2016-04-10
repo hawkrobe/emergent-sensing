edff33d3406a95a527dc7d74cf03ead75310ba2f
hawkrobe <rxdh@stanford.edu>
2016-04-09 20:36:19 -0700

    Read csvs more nicely

--- experiment2/utils.js
+++ experiment2/utils.js
@@ -1,11 +1,24 @@
+var babyparse = require("babyparse");
+var fs = require("fs");
 
 module.exports = {
-    UUID: function() {
-	var name = Math.floor(Math.random() * 10) + '' + Math.floor(Math.random() * 10) + '' + Math.floor(Math.random() * 10) + '' + Math.floor(Math.random() * 10);
-	var id = name + '-' + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  readCSV : function(filename){
+    return babyparse
+      .parse(fs.readFileSync(filename, 'utf8'),
+	     {header : true})
+      .data;
+  },
+
+  UUID: function() {
+    var name = (Math.floor(Math.random() * 10) +
+		'' + Math.floor(Math.random() * 10) +
+		'' + Math.floor(Math.random() * 10) +
+		'' + Math.floor(Math.random() * 10));
+    var id = (name + '-' + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx')
+	  .replace(/[xy]/g, function(c) {
 	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 	    return v.toString(16);
-	});
-	return id;
-    }
-}
+	  });
+    return id;
+  }
+};
