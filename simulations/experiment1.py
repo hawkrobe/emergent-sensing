9f4860af1242926e665fc6722d3ddd77d0e95ec8
Robert Hawkins <hawkrobe@gmail.com>
2021-05-17 18:36:43 -0700

    update experiment1 scripts

--- simulations/experiment1.py
+++ simulations/experiment1.py
@@ -4,8 +4,7 @@ sys.path.append('./player_model/')
 sys.path.append('./simulations/')
 sys.path.append('./utils/')
 
-import simulations.emergent_simulation as simulation
-
+import simulations.experiment1_simulation as simulation
 import simulation_utils
 import config
 import exp_config
@@ -18,30 +17,28 @@ reps = exp_config.simulation_reps
 info, out_dir = exp_config.get_emergent_config(reps)
 
 def run_simulation(exp_ind):
-
     experiment = info['experiments'][exp_ind]    
     bg = info['background_types'][exp_ind]
     nbots = info['nums_bots'][exp_ind]
     strategy = info['strategies'][exp_ind]
-    print(experiment, bg, nbots, strategy)
+
+    print('running', experiment)
     environment = get_environment(bg)
     centers = {'player': simulation_utils.random_walk_centers(environment)}
     models = [BasicBot(environment, [True]*nbots, strategy, i) for i in range(nbots)]    
     with open(out_dir + experiment + '-simulation.csv', 'w') as out_f:
         out_f.write('exp,pid,tick,active,state,x_pos,y_pos,velocity,angle,bg_val,goal_x,goal_y\n')        
-        simulation.simulate(centers, models, environment, out_f,
-                            out_dir, experiment)
+        simulation.simulate(centers, models, environment, out_f, out_dir, experiment)
 
 def get_environment(bg):
-    return lambda x: RectangularWorld(x,
-                                      config.GAME_LENGTH,
-                                      False,
-                                      config.DISCRETE_BG_RADIUS,
-                                      edge_goal = (bg == 'wall'))
-
+    return lambda x: RectangularWorld(
+        x,
+        config.GAME_LENGTH,
+        False,
+        config.DISCRETE_BG_RADIUS,
+        edge_goal = (bg == 'wall')
+    )
 
 if __name__ == '__main__':
-#    freeze_support()
    p = Pool(exp_config.num_procs)
    p.map(run_simulation, range(len(info['experiments'])))
-#map(func, range(len(info['experiments'])))
--- simulations/simulations/experiment1_simulation.py
+++ simulations/simulations/experiment1_simulation.py
@@ -1,6 +1,6 @@
-
 import sys
-sys.path.append('../player_model/')
+sys.path.append('player_model/')
+sys.path.append('simulations/')
 
 import pandas as pd
 import numpy as np
@@ -15,51 +15,37 @@ import config
 def simulate(centers, models, environment_model, out_file, out_dir, file_prefix):
     
     n_players = len(models)
-
-    world = World(environment_model, noise_location = None, n_players
-                  = n_players, stop_and_click =
-                  config.STOP_AND_CLICK)
-    
+    world = World(environment_model, noise_location = None, n_players = n_players,
+                  stop_and_click = config.STOP_AND_CLICK)
     world.world_model.centers = centers['player']
-    
     pids = range(n_players)
     
     # TODO: this is a hack to have bg_vals set properly
     world.advance() 
     world.time = 0
     
-    goals = [['',''] for i in range(n_players)]
-    
+    goals = [['',''] for i in range(n_players)]    
     for i in range(n_players):
         p = world.players[i]
-        write(pids[i], p, 0, out_file, goals[i], p.curr_background)
+        write(pids[i], p, models[i], 0, out_file, goals[i], file_prefix)
     
     for tick in range(1, world.game_length):
-        
-        models_copy = copy.deepcopy(models)
-        
-        for i in range(n_players):
-        
+        models_copy = copy.deepcopy(models)        
+        for i in range(n_players):        
             pos, bg_val, others, time = world.get_obs(i)
-                
-            models[i].observe(pos, bg_val, time)
-            
+            models[i].observe(pos, bg_val, time)            
             goals[i],slow = models[i].act(world.players[i], models_copy)
         
         world.advance()
-        
         for i in range(n_players):
-            
-            p = world.players[i]
-            
             if out_file is not None:
-                write(pids[i], p, tick, out_file, goals[i], p.curr_background)
+                write(pids[i], world.players[i], models[i], tick, out_file, goals[i], file_prefix)
     
     write_centers(world.world_model.centers, out_dir + file_prefix + '-bg.csv')
 
-def write(pid, p, tick, out_file, goal, bg_val):
-    out = [pid, tick, 'true', p.pos[0], p.pos[1], p.speed, p.angle, bg_val, '', goal[0], goal[1]]
-    out = map(str, out)
+def write(pid, p, model, tick, out_file, goal, file_prefix):
+    out = [file_prefix, pid, tick, 'true', model.state, p.pos[0], p.pos[1], p.speed, p.angle, p.curr_background, goal[0], goal[1]]
+    out = list(map(str, out))
     out_file.write(','.join(out) + '\n')
 
 def write_centers(centers, center_file):    
