a7fdbee8d7fb1171ed75a62c99e72d307b160a80
Robert Hawkins <hawkrobe@gmail.com>
2022-02-17 23:40:41 -0800

    update readme

--- simulations/README.txt
+++ simulations/README.txt
@@ -1,7 +1,9 @@
-Configurations are set by exp_config.py and config.py
+Configurations are set by config.py
 
-for simulations related to experiment 1, run `python experiment1.py`, and likewise for experiment 2 and 3.
+run `python run_simulations.py` to launch a big grid of different parameter settings
 
 to visualize a simulation run as a video, run, e.g., `sh make_video.sh output/predictions-emergent/spot-2-asocial-1-`
 
 note that you must have [`ffmpeg`](http://ffmpeg.org) installed (on mac, you can get it via homebrew with `brew install ffmpeg`)
+
+in the paper, we focus on the core qualitative predictions evaluated in exp. 1, but simulations related to the other two experiments can be found in `supplemental```
--- simulations/run_simulations.py
+++ simulations/run_simulations.py
@@ -6,8 +6,6 @@ sys.path.append('./player_model/')
 sys.path.append('./utils')
 
 import config
-import exp_config
-
 import pandas as pd
 import numpy as np
 
@@ -15,9 +13,6 @@ from multiprocessing import Pool
 from bots import BasicBot
 from rectangular_world import RectangularWorld
 from environment import *
-
-reps = exp_config.simulation_reps
-info, out_dir = exp_config.get_emergent_config(reps)
 
 def write(pid, p, model, tick, out_file, goal, experiment):
     nbots, composition, rep, prob_explore = experiment.split('-')
@@ -89,5 +84,48 @@ def simulate_tick(tick, models, world, out_file, experiment):
     
 
 if __name__ == '__main__':
-  p = Pool(exp_config.num_procs)
-  p.map(run_simulation, range(len(info['experiments'])))
+    reps = 50
+    out_dir = './output/predictions-emergent/'
+    strategies = ['naive_copy', 'smart', 'asocial', 'move_to_closest', 'move_to_center']
+    num_procs = 8
+    info = {'experiments' : [], 'bots' : [], 'strategies' : [], 'prob_explore' : []}
+    for strategy in strategies :
+        for group_size in [1,2,3,4,6,8,16,32]: # 16,32,64,128
+            for prob_explore in ([None] if strategy in ['asocial', 'smart'] else [0,.25,.5,.75]) :
+                composition = np.array([strategy == 'asocial', strategy == 'naive_copy',
+                                        strategy == 'move_to_center', strategy == 'move_to_closest',
+                                        strategy == 'smart']) * group_size
+                bots = ([{'strategy' : 'asocial', 'prob_explore' : prob_explore}] * composition[0] +
+                        [{'strategy' : 'naive_copy', 'prob_explore' : prob_explore}] * composition[1] +
+                        [{'strategy' : 'move_to_center', 'prob_explore' : prob_explore}] * composition[2] +
+                        [{'strategy' : 'move_to_closest', 'prob_explore' : prob_explore}] * composition[3] +
+                        [{'strategy' : 'smart', 'prob_explore' : prob_explore}] * composition[4])
+                info['experiments'] += ['-'.join(
+                    [str(len(bots)), '+'.join([str(i) for i in composition]), str(rep), str(prob_explore)]
+                ) for rep in range(reps)]
+                info['prob_explore'] += [prob_explore for rep in range(reps)]
+                info['bots'] += [bots for rep in range(reps)]
+                info['strategies'] += [composition for rep in range(reps)]
+
+    p = Pool(exp_config.num_procs)
+    p.map(run_simulation, range(len(info['experiments'])))
+
+    # for n_asocial in range(7) :
+    #     for n_naive in range(7 - n_asocial) :
+    #         for rep in range(reps):
+    #             n_center = 0
+    #             n_closest = 0
+    #             n_smart = 6 - n_asocial - n_naive
+    #             composition = (n_asocial, n_naive, n_center, n_closest, n_smart)
+    #             bots = ([{'strategy' : 'asocial', 'prob_explore' : 0.5}] * n_asocial +
+    #                     [{'strategy' : 'naive_copy', 'prob_explore' : 0.5}] * n_naive +
+    #                     [{'strategy' : 'move_to_center', 'prob_explore' : 0.5}] * n_center +
+    #                     [{'strategy' : 'move_to_closest', 'prob_explore' : 0.5}] * n_closest +
+    #                     [{'strategy' : 'smart', 'prob_explore' : 0.5}] * n_smart)
+    #             nbots = len(bots)
+    #             print(composition)
+    #             info['experiments'] += ['-'.join(
+    #                 [str(nbots), ''.join([str(i) for i in composition]), str(rep)]
+    #             )]
+    #             info['bots'] += [bots]
+    #             info['strategies'] += [composition]
