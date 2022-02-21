52e1b06042fcaa6a48dbe0017869cc95409d2485
Robert Hawkins <hawkrobe@gmail.com>
2022-02-20 23:29:02 -0800

    add zip

--- data/experiment1/aggregate_games.py
+++ data/experiment1/aggregate_games.py
@@ -1,30 +1,38 @@
 import sys
 import csv
 
+import pandas as pd
+
 sys.path.append("../utils/")
 from game_utils import *
 
-data_dir = './'
-games = []
-games += get_games(data_dir, 'experiment-exploratory-2016')
-games += get_games(data_dir, 'experiment-confirmatory-2016')
+data_dirs = []
+data_dirs += get_games('./raw_data/', 'experiment-exploratory-2016')
+data_dirs += get_games('./raw_data/', 'experiment-confirmatory-2016')
 
+# aggregate raw game data
+games = []
+inactive = []
 raw_data = []
-for data_dir in games :
+for data_dir in data_dirs :
     for game in os.listdir(data_dir + '/games'):
         data = pd.read_csv(data_dir + '/games/' + game)
+        games += [data_dir + '/games/' + game]
         data['gameid'] = game
+        inactive += [get_inactive(data_dir, game)]
         raw_data.append(data.copy())
-pd.concat(raw_data).to_csv('./raw_games/all_raw_games.csv')
 
-# data = get_data(data_dir, games)
+pd.concat(raw_data).to_csv('./raw_data/all_raw_games.csv')
 
-# inactive = [get_inactive(data_dir, game) for game in games]
-# inactive = {k: v for d in inactive for k, v in d.items()}
+# find inactive games
+inactive = {k: v for d in inactive for k, v in d.items()}
+print(inactive.keys())
+writer = csv.writer(open('./inactive_games.csv','w'))
+for pid in list(inactive.keys()):
+    writer.writerow([pid])
 
-# writer = csv.writer(open('./inactive_games.csv','w'))
-
-# for pid in list(inactive.keys()):
-#     writer.writerow([pid])
-
-# data.to_csv('./all_games.csv')
+# # get 'cleaned' games
+# dfs = []
+# for data_dir in data_dirs :
+#     dfs += [get_data(data_dir, games)]
+# pd.concat(dfs).to_csv('./all_games_new.csv')
